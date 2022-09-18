d4a36b205e4b675c7d154119feb991ef335a8fcb
Shashankranjan-dev <shashankranjan970832@gmail.com>
2022-09-19 01:34:50 +0530

    full stack

---
+++ client/home/homescript.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".typing", {
+    strings: ["Phones.", "Clothes.", "Watches.", "Books."],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+// https://mattboldt.com/demos/typed-js/
+
+// https://github.com/mattboldt/typed.js/
---
+++ client/script.js
@@ -0,0 +1,137 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+
+function userFormSubmit() {
+  var username = document.getElementById("username-signup").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password-signup").value;
+
+  if (username == "") {
+    Swal.fire({
+      icon: "error",
+      title: "Error!",
+      text: "Username is required!",
+    });
+    return false;
+  }
+  if (email == "") {
+    Swal.fire({
+      icon: "error",
+      title: "Error!",
+      text: "Email is required!",
+    });
+    return false;
+  }
+  if (password == "") {
+    Swal.fire({
+      icon: "error",
+      title: "Error!",
+      text: "Password is required!",
+    });
+    return false;
+  } else {
+    var user = JSON.stringify({
+      username: username,
+      email: email,
+      password: password,
+    });
+
+    fetch("http://localhost:3000/api/user/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: user,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Data:", data);
+        if (data.statusCode == 201) {
+          Swal.fire({
+            icon: "success",
+            title: "Success!",
+            text: "User Registered Successfully!",
+            confirmButtonColor: "#3085d6",
+            confirmButtonText: "Ok",
+          })
+          // remove Animation
+          .then((result) => {
+            if (result.isConfirmed) {
+              container.classList.remove("sign-up-mode");
+            }
+          });
+        }
+      })
+      .catch((error) => {
+        Swal.fire({
+          icon: "error",
+          title: "Error!",
+          text: error,
+        });
+        console.error("Error:", error);
+      });
+
+    console.log("User: ", user);
+  }
+}
+
+function userLogin() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  if (username == "") {
+    Swal.fire({
+      icon: "error",
+      title: "Error!",
+      text: "Username is required!",
+    });
+    return false;
+  }
+  if (password == "") {
+    Swal.fire({
+      icon: "error",
+      title: "Error!",
+      text: "Password is required!",
+    });
+    return false;
+  } else {
+    var user = JSON.stringify({
+      username: username,
+      password: password,
+    });
+
+    fetch("http://localhost:3000/api/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: user,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Login Data:", data);
+        if (data.statusCode == 200) {
+          window.location.href = "/Project/client/home/home.html";
+        }
+      })
+      .catch((error) => {
+        Swal.fire({
+          icon: "error",
+          title: "Error!",
+          text: error,
+        });
+        console.error("Error:", error);
+      });
+
+    console.log("User: ", user);
+  }
+}
